706feb20958d8358cc59d6c1ad949a332dd6da4e
iamayush25 <ayush22@navgurukul.org>
2024-02-21 23:40:45 +0530

    commit

--- Responsive News Project/News_paper.js
+++ Responsive News Project/News_paper.js
@@ -1,30 +1,29 @@
 async function getTopHeadlines() {
-    const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0d4d93237b364b27aed4c48768350fdd')
-    const data = await response.json();
-    const articles = await data.articles;
-    console.log(articles);
-    displayArticles(articles);
+        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0d4d93237b364b27aed4c48768350fdd');
+        const data = await response.json();
+        displayArticles(data.articles);
 }
+
+
 function displayArticles(articles) {
     const newsContainer = document.getElementById("newsContainer");
     newsContainer.innerHTML = "";
-
     articles.map((article) => {
         newsContainer.innerHTML += `
         <div class="newsArticle">
-            <img src="${article.urlToImage}" alt="${article.source.name}" />
+            <img src="${article.urlToImage}" onerror="this.src='Error-Img.png';" />
             <h2>${article.title}</h2>
             <p>${article.description}</p>
             <h3> Read full artical <a target = "_blank" href = "${article.url}">&#8594;</a></h3>
     </div>`
     })
 }
-async function searchNews() {     
-    const searchInput =
-        document.getElementById("searchInput").value;
+
+
+async function searchNews() {
+    const searchInput = document.getElementById("searchInput").value.trim();
         const response = await fetch(`https://newsapi.org/v2/everything?q=${searchInput}&apiKey=0d4d93237b364b27aed4c48768350fdd`);
         const data = await response.json();
-        const articles = await data.articles;
-        displayArticles(articles);
+        displayArticles(data.articles);
 }
 getTopHeadlines();
